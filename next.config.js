6b44488e7a791f4a70580b7d1a1e694b677b79da
JIN <andyzju@users.noreply.github.com>
2023-11-16 01:33:54 +0800

    Update next.config.js
    added init infor

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
+  rootNotionPageId:'a5270c2b80ed4f4ba809a605009e7d04',
+  rootNotionSpaceId:null,
+  name:'阿拉丁禅意花园',
+  domain:'https://decorous-feverfew-513.notion.site/Teamspace-Home-a5270c2b80ed4f4ba809a605009e7d04?pvs=4',
+  author:'阿拉丁',
+  douyin:'阿拉丁神灯能量',
+  xiaohongshu:'阿拉丁神灯能量',
   images: {
     domains: [
       'www.notion.so',
